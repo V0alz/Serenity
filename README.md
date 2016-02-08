fcb7abcd38bc7b37e44e111d57c921389edff87a
V0alz <v0alzgames@gmail.com>
2016-02-08 19:44:06 +0000

    Added license
    I actually forgot the license... :smile: :hand:

---
+++ README.md
@@ -0,0 +1,7 @@
+### Serenity
+Serenity is my main project.  
+Multiple of my attempts at voxel engines have failed - __This seems promising though__  
+I'll update this soon with details.
+
+#### License
+See the LICENSE(.md) file for more details.
--- src/Renderer/renderer.d
+++ src/Renderer/renderer.d
@@ -1,9 +1,26 @@
+/*
+*	Serenity - A personal voxel game.
+*	Copyright(C) 2015-2016 Dennis Walsh
+*
+*	This program is free software : you can redistribute it and / or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
 module s.renderer.renderer;
 import std.stdio;
 import derelict.opengl3.gl3;
 import derelict.glfw3.glfw3;
 import gl3n.linalg : vec3;
-private import s.renderer.window;
+public import s.renderer.window;
 import s.renderer.shader;
 import s.renderer.camera;
 import s.system.logger;
